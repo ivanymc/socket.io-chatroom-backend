30759b0ce2419d4d55efd109ab7e244201a3bfad
Ivan Chan Yin Man <scskchan@gmail.com>
2022-09-28 00:59:39 +0800

    fix static

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(morgan('tiny'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname, 'public'));
+app.use(express.static('public'));
 
 // Connect to MongoDB
 mongoose.connect(process.env.DB_URL)
